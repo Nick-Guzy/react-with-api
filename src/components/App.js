c832fe09bff05f2ec176805dff87518c33de5e26
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-08 14:40:29 -0700

    Set up API returning Top Stories

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TopStories from './TopStories';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      < TopStories />
+    </React.Fragment>
+  );
+}
+
+
+export default App;
